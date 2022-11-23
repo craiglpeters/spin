0606e511c42656f55fc41d211ce2a098bb52e3e5
Craig Peters <craiglpeters@github.com>
2022-11-23 15:40:48 +0000

    fix the spin feature

--- .devcontainer/spin/install.sh
+++ .devcontainer/spin/install.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
-cd /usr/local/bin ; sudo curl https://spin.fermyon.dev/downloads/install.sh | sudo bash
+VERSION="v0.6.0"
+OSARCH="linux-amd64"
+
+wget https://github.com/fermyon/spin/releases/download/${VERSION}/spin-${VERSION}-${OSARCH}.tar.gz
+tar xfv spin-${VERSION}-${OSARCH}.tar.gz
+su
+mv ./spin /usr/local/bin/
